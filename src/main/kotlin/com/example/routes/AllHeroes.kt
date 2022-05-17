bcb16d6083180f3acc3db3d231606c5f41b9d762
Fromssa <Fromssa.olana@gmail.com>
2022-05-17 03:10:13 -0500

    Implemented getAllHeroes method in the HeroRepositoryImpl.kt and added to Router

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,24 +1,33 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 
-fun Route.getAllHeroes(){
+fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get("/anime/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
-           call.respond(message = page)
-        } catch (e: NumberFormatException){
+            val apiResponse = heroRepository.getAllHeroes(page)
+
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+        } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(success = false, message = "only Numbers please"),
                 status = HttpStatusCode.BadRequest
             )
-        } catch (e: IllegalArgumentException){
+        } catch (e: IllegalArgumentException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Heroes not found"),
                 status = HttpStatusCode.NotFound
